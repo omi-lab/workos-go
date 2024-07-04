a3672ea2db361b8fe03b15f0ed05c9008a8b4245
Elliot Maincourt <e.maincourt@gmail.com>
2024-07-04 13:50:29 +0200

    chore(models): add support for connection webhooks

--- pkg/models/webhooks.go
+++ pkg/models/webhooks.go
@@ -7,6 +7,9 @@ type WebhookEventName = string
 const (
 	WebhookEventNameEmailVerificationCreated WebhookEventName = "email_verification.created"
 	WebhookEventNamePasswordResetCreated     WebhookEventName = "password_reset.created"
+	WebhookEventNameConnectionActivated      WebhookEventName = "connection.activated"
+	WebhookEventNameConnectionDeactivated    WebhookEventName = "connection.deactivated"
+	WebhookEventNameConnectionDeleted        WebhookEventName = "connection.deleted"
 )
 
 type WebhookEvent struct {
@@ -37,5 +40,55 @@ type WebhookEventPasswordResetCreated struct {
 		UserID    string    `json:"user_id"`
 		ExpiresAt time.Time `json:"expires_at"`
 		CreatedAt time.Time `json:"created_at"`
+	}
+}
+
+type WebhookEventConnectionDomain struct {
+	Object string `json:"object"`
+	ID     string `json:"id"`
+	Domain string `json:"domain"`
+}
+
+type WebhookEventConnectionActivated struct {
+	WebhookEvent
+	Data struct {
+		Object         string                         `json:"object"`
+		ID             string                         `json:"id"`
+		OrganizationID string                         `json:"organization_id"`
+		ExternalKey    string                         `json:"external_key"`
+		State          ConnectionState                `json:"state"`
+		Status         ConnectionStatus               `json:"status"`
+		Domains        []WebhookEventConnectionDomain `json:"domains"`
+		ExpiresAt      time.Time                      `json:"expires_at"`
+		CreatedAt      time.Time                      `json:"created_at"`
+	}
+}
+
+type WebhookEventConnectionDeactivated struct {
+	WebhookEvent
+	Data struct {
+		Object         string                         `json:"object"`
+		ID             string                         `json:"id"`
+		OrganizationID string                         `json:"organization_id"`
+		ExternalKey    string                         `json:"external_key"`
+		State          ConnectionState                `json:"state"`
+		Status         ConnectionStatus               `json:"status"`
+		Domains        []WebhookEventConnectionDomain `json:"domains"`
+		ExpiresAt      time.Time                      `json:"expires_at"`
+		CreatedAt      time.Time                      `json:"created_at"`
+	}
+}
+
+type WebhookEventConnectionDeleted struct {
+	WebhookEvent
+	Data struct {
+		Object         string           `json:"object"`
+		ID             string           `json:"id"`
+		OrganizationID string           `json:"organization_id"`
+		ExternalKey    string           `json:"external_key"`
+		State          ConnectionState  `json:"state"`
+		Status         ConnectionStatus `json:"status"`
+		ExpiresAt      time.Time        `json:"expires_at"`
+		CreatedAt      time.Time        `json:"created_at"`
 	}
 }
